29d2b0cf7c568154c5b9df3e8f50c13c83b6e711
justinvdm <justnvdm@gmail.com>
2018-11-11 10:12:36 +0100

    implement basics

--- index.js
+++ index.js
@@ -10,4 +10,76 @@
   }
 })(this || 0, function () {
   'use strict'
+
+  var isArray = Array.isArray
+  return unrollElement
+
+  function unrollElement (el, resolveFn) {
+    var type
+    var children
+
+    if (isArray(el)) {
+      return unrollElementList(el, resolveFn)
+    }
+
+    if (!el || typeof el !== 'object') {
+      return el
+    }
+
+    type = el.type
+
+    if (typeof type === 'function') {
+      return unrollElement(type(el.props), resolveFn)
+    }
+
+    children = el.props.children
+
+    if (type && type.toString() === 'Symbol(react.fragment)') {
+      return unrollElement(children, resolveFn)
+    }
+
+    return resolveFn(el, flatten(unrollElement(children, resolveFn)))
+  }
+
+  function unrollElementList (els, resolveFn) {
+    var n = els.length
+    var i = -1
+    var res = []
+    while (++i < n) res.push(unrollElement(els[i], resolveFn))
+    return new List(res)
+  }
+
+  function flatten (obj) {
+    if (!(obj instanceof List)) {
+      return obj
+    }
+
+    var res = []
+    var vals = obj.vals
+    var n = vals.length
+    var i = -1
+    var v
+
+    while (++i < n) {
+      v = vals[i]
+
+      if (v instanceof List) {
+        pushMany(res, flatten(v.vals))
+      } else {
+        res.push(v)
+      }
+    }
+
+    return res
+  }
+
+  function pushMany (collection, vals) {
+    var n = vals.length
+    var i = -1
+    while (++i < n) collection.push(vals[i])
+  }
+
+  function List (vals) {
+    this.vals = vals
+  }
 })
--- test.js
+++ test.js
@@ -3,3 +3,87 @@ const React = require('react')
 const test = require('tape')
 
 const unrollElement = require('.')
+
+const resolveFn = (el, content) => {
+  const { type: kind, props: { children: _children, ...props } } = el
+  return [kind, props, content]
+}
+
+test('host elements', t => {
+  const testEl = (
+    <foo a={2}>
+      <bar>21</bar>
+      <bar>23</bar>
+    </foo>
+  )
+
+  const res = unrollElement(testEl, resolveFn)
+
+  t.deepEquals(res, ['foo', { a: 2 }, [['bar', {}, '21'], ['bar', {}, '23']]])
+  t.end()
+})
+
+test('function components', t => {
+  const TestComponent = ({ children, a }) => <bar a={a}>{children}</bar>
+
+  const testEl = (
+    <foo>
+      <TestComponent a={2}>
+        <baz>21</baz>
+      </TestComponent>
+    </foo>
+  )
+
+  const res = unrollElement(testEl, resolveFn)
+
+  t.deepEquals(res, ['foo', {}, ['bar', { a: 2 }, ['baz', {}, '21']]])
+  t.end()
+})
+
+test('array-returning components', t => {
+  const TestComponent = () => [<bar>2</bar>, [<baz>3</baz>, <quux>23</quux>]]
+
+  const testEl = (
+    <foo>
+      <TestComponent />
+    </foo>
+  )
+
+  const res = unrollElement(testEl, resolveFn)
+
+  t.deepEquals(res, [
+    'foo',
+    {},
+    [['bar', {}, '2'], ['baz', {}, '3'], ['quux', {}, '23']]
+  ])
+
+  t.end()
+})
+
+test('fragment-returning components', t => {
+  const TestComponent = () => (
+    <React.Fragment>
+      <bar>2</bar>
+      <React.Fragment>
+        <baz>3</baz>
+        <quux>23</quux>
+      </React.Fragment>
+    </React.Fragment>
+  )
+
+  const testEl = (
+    <foo>
+      <TestComponent />
+    </foo>
+  )
+
+  const res = unrollElement(testEl, resolveFn)
+
+  t.deepEquals(res, [
+    'foo',
+    {},
+    [['bar', {}, '2'], ['baz', {}, '3'], ['quux', {}, '23']]
+  ])
+
+  t.end()
+})
